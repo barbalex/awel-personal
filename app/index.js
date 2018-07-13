610b78b48f372d070c23eb00c714099089c724d6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-13 10:45:41 +0200

    access data in db

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import Database from 'better-sqlite3';
 
 import App from './components/App';
+
+const db = new Database('C:/Users/alexa/kapla.db');
+const geschaefte = db
+  .prepare('SELECT idGeschaeft from geschaefte limit 1')
+  .get();
+console.log('geschaefte', geschaefte);
 
 render(
   <AppContainer>
