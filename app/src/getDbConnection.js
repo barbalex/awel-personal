aed5534eefd54b5d12297e8fc86c205fa5e47b19
Alexander Gabriel <alex.barbalex@gmail.com>
2018-07-14 21:25:09 +0200

    save dbPath in config file

--- app/src/getDbConnection.js
+++ app/src/getDbConnection.js
@@ -1,10 +1,13 @@
 import Database from 'better-sqlite3'
 
 import chooseDb from './chooseDb'
+import getConfig from './getConfig'
+import saveConfig from './saveConfig'
 
 export default async () => {
+  const config = getConfig()
+  let dbPath = config.dbPath || 'C:/Users/alexa/kapla.db'
   let db
-  let dbPath = 'C:/Users/alexa/kapla.db'
   try {
     db = new Database(dbPath, { fileMustExist: true })
   } catch (error) {
@@ -16,6 +19,8 @@ export default async () => {
         return console.log('Error after choosing db:', chooseError)
       }
       db = new Database(dbPath, { fileMustExist: true })
+      config.dbPath = dbPath
+      saveConfig(config)
     } else {
       return console.log('index.js, Error opening db file:', error)
     }
---
+++ app/src/saveConfig.js
@@ -0,0 +1,14 @@
+// see: http://www.mylifeforthecode.com/saving-and-restoring-window-state-in-electron/
+import electron from 'electron'
+import fs from 'fs'
+import path from 'path'
+
+const { app } = electron.remote
+
+const dataFilePath = path.join(
+  app.getPath('userData'),
+  'awelPersonalConfig.json'
+)
+
+export default data =>
+  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2))
