40577a7722807f86d85bbfe60db636ea79c84460
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-14 11:52:17 +0200

    begin creating tables

---
+++ app/sql/createTables.sql
@@ -0,0 +1,13 @@
+drop table if exists person;
+create table person (
+  id integer primary key autoincrement,
+  name text,
+  vorname text,
+  kurzzeichen text,
+  telefon_nr text,
+  telefonnr_mobile text,
+  buero text,
+  org_einheit text,
+  kostenstelle text,
+  vorgesetzt_id integer references person(id) on update cascade on delete set null,
+)
