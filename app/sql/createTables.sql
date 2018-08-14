88d1ecc519b33dea16e271cbe14f52e8fcb15ae6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-14 11:55:25 +0200

    add fields

--- app/sql/createTables.sql
+++ app/sql/createTables.sql
@@ -10,4 +10,6 @@ create table person (
   org_einheit text,
   kostenstelle text,
   vorgesetzt_id integer references person(id) on update cascade on delete set null,
+  eintritt_datum text,
+  austritt_datum text,
 )
