6d90f67a4413af842bbd50890c4facc698195f02
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-16 22:32:24 +0200

    only show form if active id exists

--- app/components/PersonContainer/index.js
+++ app/components/PersonContainer/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import compose from 'recompose/compose'
 import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 import styled from 'styled-components'
+import { inject, observer } from 'mobx-react'
 
 import ErrorBoundary from '../shared/ErrorBoundary'
 import Person from './Person'
@@ -15,31 +16,39 @@ const Container = styled.div`
 // above does not work
 // seems that navbar is not finished when PersonContainer is built
 
-const enhance = compose()
-
-const PersonContainer = () => (
-  <Container>
-    <ErrorBoundary>
-      <ReflexContainer orientation="vertical">
-        <ReflexElement
-          flex={0.33}
-          propagateDimensions
-          renderOnResizeRate={100}
-          renderOnResize
-        >
-          <List />
-        </ReflexElement>
-        <ReflexSplitter />
-        <ReflexElement
-          propagateDimensions
-          renderOnResizeRate={100}
-          renderOnResize
-        >
-          <Person />
-        </ReflexElement>
-      </ReflexContainer>
-    </ErrorBoundary>
-  </Container>
+const enhance = compose(
+  inject('store'),
+  observer
 )
+
+const PersonContainer = ({ store }: { store: Object }) => {
+  const location = store.location.toJSON()
+  const activeId = location[1]
+
+  return (
+    <Container>
+      <ErrorBoundary>
+        <ReflexContainer orientation="vertical">
+          <ReflexElement
+            flex={0.33}
+            propagateDimensions
+            renderOnResizeRate={100}
+            renderOnResize
+          >
+            <List />
+          </ReflexElement>
+          <ReflexSplitter />
+          <ReflexElement
+            propagateDimensions
+            renderOnResizeRate={100}
+            renderOnResize
+          >
+            {activeId && <Person />}
+          </ReflexElement>
+        </ReflexContainer>
+      </ErrorBoundary>
+    </Container>
+  )
+}
 
 export default enhance(PersonContainer)
