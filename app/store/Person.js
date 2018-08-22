41297553e3f1f43a21df208d1d6a83c400e01057
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-22 19:06:55 +0200

    enable undefined values

--- app/store/Person.js
+++ app/store/Person.js
@@ -3,24 +3,46 @@ import { types } from 'mobx-state-tree'
 export default types.model('Person', {
   id: types.integer,
   deleted: types.optional(types.integer, 0),
-  name: types.union(types.string, types.integer, types.null),
-  vorname: types.union(types.string, types.integer, types.null),
-  kurzzeichen: types.union(types.string, types.integer, types.null),
-  telefonNr: types.union(types.string, types.integer, types.null),
-  telefonNrMobile: types.union(types.string, types.integer, types.null),
-  email: types.union(types.string, types.integer, types.null),
-  geburtDatum: types.union(types.string, types.integer, types.null),
-  bueroNr: types.union(types.string, types.integer, types.null),
-  abteilung: types.union(types.string, types.integer, types.null),
-  kostenstelle: types.union(types.string, types.integer, types.null),
+  name: types.maybe(types.union(types.string, types.integer, types.null)),
+  vorname: types.maybe(types.union(types.string, types.integer, types.null)),
+  kurzzeichen: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  telefonNr: types.maybe(types.union(types.string, types.integer, types.null)),
+  telefonNrMobile: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  email: types.maybe(types.union(types.string, types.integer, types.null)),
+  geburtDatum: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  bueroNr: types.maybe(types.union(types.string, types.integer, types.null)),
+  abteilung: types.maybe(types.union(types.string, types.integer, types.null)),
+  kostenstelle: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
   vorgesetztId: types.maybeNull(types.integer),
-  eintrittDatum: types.union(types.string, types.integer, types.null),
-  austrittDatum: types.union(types.string, types.integer, types.null),
-  status: types.union(types.string, types.integer, types.null),
-  parkplatzNr: types.union(types.string, types.integer, types.null),
-  parkplatzBeitrag: types.union(types.string, types.integer, types.null),
-  geschlecht: types.union(types.string, types.integer, types.null),
-  bemerkungen: types.union(types.string, types.integer, types.null),
-  letzteMutationZeit: types.union(types.string, types.integer, types.null),
-  letzteMutationUser: types.union(types.string, types.integer, types.null)
+  eintrittDatum: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  austrittDatum: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  status: types.maybe(types.union(types.string, types.integer, types.null)),
+  parkplatzNr: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  parkplatzBeitrag: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  geschlecht: types.maybe(types.union(types.string, types.integer, types.null)),
+  bemerkungen: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  letzteMutationZeit: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  ),
+  letzteMutationUser: types.maybe(
+    types.union(types.string, types.integer, types.null)
+  )
 })
