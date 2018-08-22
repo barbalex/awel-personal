4ab95bf757faa43bb91e521aa8946ecaa5e8c76f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-22 10:26:49 +0200

    work on stammdaten

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,12 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
+import compose from 'recompose/compose'
+import { inject, observer } from 'mobx-react'
 
 import Navbar from './Navbar'
 import PersonContainer from './PersonContainer'
+import StammdatenContainer from './StammdatenContainer'
 import DeletionModal from './DeletionModal'
 
 const Container = styled.div`
@@ -11,12 +14,21 @@ const Container = styled.div`
   overflow: hidden;
 `
 
-const App = () => (
-  <Container>
-    <Navbar />
-    <PersonContainer />
-    <DeletionModal />
-  </Container>
+const enhance = compose(
+  inject('store'),
+  observer
 )
 
-export default App
+const App = ({ store }: { store: Object }) => {
+  const activeLocation = store.location.toJSON()[0]
+  return (
+    <Container>
+      <Navbar />
+      {activeLocation === 'Personen' && <PersonContainer />}
+      {activeLocation !== 'Personen' && <StammdatenContainer />}
+      <DeletionModal />
+    </Container>
+  )
+}
+
+export default enhance(App)
