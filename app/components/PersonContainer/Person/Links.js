728e22d79e91bbfd884fa043bab2ec1ebfb3cd3c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-08-25 11:20:13 +0200

    work on links

--- app/components/PersonContainer/Person/Links.js
+++ app/components/PersonContainer/Person/Links.js
@@ -6,31 +6,24 @@ import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
 import styled from 'styled-components'
+import { Col, FormGroup, Label } from 'reactstrap'
 
 import ifIsNumericAsNumber from '../../../src/ifIsNumericAsNumber'
 
 const Container = styled.div`
   grid-area: areaLinks;
   background-color: ${props =>
-    props['data-ispdf'] ? 'rgb(227, 232, 255)' : '#e3fff0'};
+    props['data-ispdf'] ? 'rgba(0, 0, 0,0)' : 'rgba(0, 0, 0,0)'};
   display: grid;
   grid-template-columns: ${props =>
-    props['data-ispdf'] ? '100%' : 'calc(100% - 308px) 300px'};
+    props['data-ispdf'] ? '1fr' : '1fr 1fr 1fr'};
   grid-template-areas: ${props =>
-    props['data-ispdf']
-      ? "'title' 'links'"
-      : "'title dropzone' 'links dropzone'"};
+    props['data-ispdf'] ? "'links'" : "'links links dropzone'"};
   grid-column-gap: 8px;
   grid-row-gap: ${props => (props['data-ispdf'] ? '1px' : '8px')};
-  padding: 8px;
   border: ${props => (props['data-ispdf'] ? '1px solid #ccc' : 'none')};
   border-bottom: none;
   font-size: ${props => (props['data-ispdf'] ? '10px' : 'inherit')};
-`
-const Title = styled.div`
-  font-weight: 900;
-  font-size: 16px;
-  grid-area: title;
 `
 const Links = styled.div`
   grid-area: links;
@@ -42,15 +35,15 @@ const Field = styled.div`
   display: grid;
   grid-template-columns: calc(100% - 20px) 20px;
   grid-gap: 0;
-  border-bottom: thin solid #cecbcb;
+  border-bottom: thin solid #cccccc;
   padding: 3px;
   align-items: center;
   min-height: ${props => (props['data-ispdf'] ? 0 : '35px')};
   &:first-of-type {
-    border-top: thin solid #cecbcb;
+    border-top: thin solid #cccccc;
   }
   &:hover {
-    background-color: #ceffe5;
+    background-color: rgba(0, 0, 0, 0.05);
   }
 `
 const UrlDiv = styled.div`
@@ -81,10 +74,13 @@ const DropzoneInnerDiv = styled.div`
   width: 100%;
   height: 100%;
   border-width: 2px;
-  border-color: #666;
+  border-color: #cccccc;
   border-style: dashed;
   border-radius: 5px;
   padding: 5px;
+`
+const StyledA = styled.a`
+  color: black;
 `
 
 const enhance = compose(
@@ -111,60 +107,66 @@ const LinksComponent = ({
   const isPdf = location[0] === 'personPdf'
 
   return (
-    <Container data-ispdf={isPdf}>
-      <Title>Links</Title>
-      <Links data-ispdf={isPdf}>
-        {myLinks.map(link => (
-          <Field key={`${link.idPerson}${link.url}`} data-ispdf={isPdf}>
-            <UrlDiv>
-              <a
-                href={link.url}
-                onClick={event => {
-                  event.preventDefault()
-                  shell.openItem(link.url)
-                }}
-              >
-                {link.url}
-              </a>
-            </UrlDiv>
-            <RemoveGlyphiconDiv
-              data-ispdf={isPdf}
-              onClick={() => store.deleteLink(link.id)}
-              title="Link entfernen"
-              id={link.id}
-            >
-              <i className="fas fa-times" />
-            </RemoveGlyphiconDiv>
-          </Field>
-        ))}
-      </Links>
-      <DropzoneContainer data-ispdf={isPdf}>
-        <StyledDropzone onDrop={onDrop}>
-          {({ isDragActive, isDragReject }) => {
-            if (isDragActive) {
-              return (
-                <DropzoneInnerDiv>
-                  <div>jetzt fallen lassen...</div>
-                </DropzoneInnerDiv>
-              )
-            }
-            if (isDragReject) {
-              return (
-                <DropzoneInnerDiv>
-                  <div>Hm. Da ging etwas schief :-(</div>
-                </DropzoneInnerDiv>
-              )
-            }
-            return (
-              <DropzoneInnerDiv>
-                <div>Datei hierhin ziehen...</div>
-                <div>...oder klicken, um sie zu wählen.</div>
-              </DropzoneInnerDiv>
-            )
-          }}
-        </StyledDropzone>
-      </DropzoneContainer>
-    </Container>
+    <FormGroup row>
+      <Label for="links" sm={2}>
+        Links
+      </Label>
+      <Col sm={10}>
+        <Container data-ispdf={isPdf} name="links">
+          <Links data-ispdf={isPdf}>
+            {myLinks.map(link => (
+              <Field key={`${link.idPerson}${link.url}`} data-ispdf={isPdf}>
+                <UrlDiv>
+                  <StyledA
+                    href={link.url}
+                    onClick={event => {
+                      event.preventDefault()
+                      shell.openItem(link.url)
+                    }}
+                  >
+                    {link.url}
+                  </StyledA>
+                </UrlDiv>
+                <RemoveGlyphiconDiv
+                  data-ispdf={isPdf}
+                  onClick={() => store.deleteLink(link.id)}
+                  title="Link entfernen"
+                  id={link.id}
+                >
+                  <i className="fas fa-times" />
+                </RemoveGlyphiconDiv>
+              </Field>
+            ))}
+          </Links>
+          <DropzoneContainer data-ispdf={isPdf}>
+            <StyledDropzone onDrop={onDrop}>
+              {({ isDragActive, isDragReject }) => {
+                if (isDragActive) {
+                  return (
+                    <DropzoneInnerDiv>
+                      <div>jetzt fallen lassen...</div>
+                    </DropzoneInnerDiv>
+                  )
+                }
+                if (isDragReject) {
+                  return (
+                    <DropzoneInnerDiv>
+                      <div>Hm. Da ging etwas schief :-(</div>
+                    </DropzoneInnerDiv>
+                  )
+                }
+                return (
+                  <DropzoneInnerDiv>
+                    <div>Datei hierhin ziehen...</div>
+                    <div>...oder klicken, um sie zu wählen.</div>
+                  </DropzoneInnerDiv>
+                )
+              }}
+            </StyledDropzone>
+          </DropzoneContainer>
+        </Container>
+      </Col>
+    </FormGroup>
   )
 }
 
