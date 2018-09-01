fb317b595ffef0125dabdeb388818c5cde3a0080
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-01 23:46:27 +0200

    comment

--- app/components/Navbar/Export/personenPrepareData.js
+++ app/components/Navbar/Export/personenPrepareData.js
@@ -1,7 +1,5 @@
 // @flow
 
-// TODO: add filter
 // TODO: add data referenced data
-// TODO: only show deleted if showDeleted
 export default ({ store }: { store: Object }) =>
   store.personenFiltered.slice().map(p => p)
