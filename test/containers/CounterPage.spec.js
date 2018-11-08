37e1ea515bd1e9fb483fa6b7c284689670995129
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-08 14:39:54 +0100

    update deps

--- test/containers/CounterPage.spec.js
+++ test/containers/CounterPage.spec.js
@@ -3,7 +3,6 @@ import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import { ConnectedRouter } from 'connected-react-router';
 import CounterPage from '../../app/containers/CounterPage';
 import { configureStore } from '../../app/store/configureStore';
 
@@ -14,9 +13,7 @@ function setup(initialState) {
   const history = createBrowserHistory();
   const provider = (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
         <CounterPage />
-      </ConnectedRouter>
     </Provider>
   );
   const app = mount(provider);
