a1cd879aed9c9c3bb042d165f56f050861b87e9c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-09 14:49:09 +0100

    use hooks

--- app/components/shared/InputWithoutLabel.js
+++ app/components/shared/InputWithoutLabel.js
@@ -1,21 +1,36 @@
 // @flow
-import React from 'react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
-import withState from 'recompose/withState'
-import withLifecycle from '@hocs/with-lifecycle'
-import { inject, observer } from 'mobx-react'
+import React, { useContext, useState, useCallback, useEffect } from 'react'
+import { observer } from 'mobx-react'
 import { Input } from 'reactstrap'
 
-const enhance = compose(
-  inject('store'),
-  withState(
-    'stateValue',
-    'setStateValue',
-    ({ value }) => (value || value === 0 ? value : '')
-  ),
-  withHandlers({
-    onBlur: ({ saveToDb, field, value }) => event => {
+import storeContext from '../../storeContext'
+
+const SharedInputWithoutLabel = ({
+  value,
+  field,
+  type = 'text',
+  rows = 1,
+  placeholder = '',
+  disabled = false,
+  saveToDb
+}: {
+  value: number | string,
+  field: string,
+  type?: string,
+  rows?: number,
+  placeholder?: string,
+  disabled?: boolean,
+  saveToDb: () => void
+}) => {
+  const store = useContext(storeContext)
+  const { showFilter } = store
+
+  const [stateValue, setStateValue] = useState(
+    value || value === 0 ? value : ''
+  )
+
+  const onBlur = useCallback(
+    event => {
       let newValue = event.target.value
       // save nulls if empty
       if (newValue === '') newValue = null
@@ -23,60 +38,37 @@ const enhance = compose(
       if (!newValue && !value && value !== 0 && newValue !== 0) return
       if (newValue === value) return
       saveToDb({ value: newValue, field })
-    }
-  }),
-  withHandlers({
-    onChange: ({ setStateValue, store, onBlur }) => event => {
+    },
+    [field, value]
+  )
+  const onChange = useCallback(
+    event => {
       setStateValue(event.target.value)
-      if (store.showFilter) {
+      if (showFilter) {
         // call onBlur to immediately update filters
         onBlur(event)
       }
-    }
-  }),
-  withLifecycle({
-    // need this check because of filtering:
-    // when filter is emptied, value needs to reset
-    onDidUpdate(prevProps, props) {
-      if (props.value !== prevProps.value) {
-        const { setStateValue, value } = props
-        setStateValue(value || value === 0 ? value : '')
-      }
-    }
-  }),
-  observer
-)
+    },
+    [showFilter]
+  )
 
-const SharedInputWithoutLabel = ({
-  stateValue,
-  field,
-  type = 'text',
-  rows = 1,
-  placeholder = '',
-  disabled = false,
-  onChange,
-  onBlur
-}: {
-  stateValue: number | string,
-  field: string,
-  type?: string,
-  rows?: number,
-  placeholder?: string,
-  disabled?: boolean,
-  onChange: () => void,
-  onBlur: () => void
-}) => (
-  <Input
-    id={field}
-    type={type}
-    name={field}
-    placeholder={placeholder}
-    disabled={disabled}
-    value={stateValue}
-    onChange={onChange}
-    onBlur={onBlur}
-    rows={rows}
-  />
-)
+  // need this check because of filtering:
+  // when filter is emptied, value needs to reset
+  useEffect(() => setStateValue(value || value === 0 ? value : ''), [value])
 
-export default enhance(SharedInputWithoutLabel)
+  return (
+    <Input
+      id={field}
+      type={type}
+      name={field}
+      placeholder={placeholder}
+      disabled={disabled}
+      value={stateValue}
+      onChange={onChange}
+      onBlur={onBlur}
+      rows={rows}
+    />
+  )
+}
+
+export default observer(SharedInputWithoutLabel)
