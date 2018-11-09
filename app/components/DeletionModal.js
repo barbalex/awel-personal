2bda31b3a518111b8683d9df6ecdf68adb79f2a3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-09 10:38:25 +0100

    use hooks

--- app/components/DeletionModal.js
+++ app/components/DeletionModal.js
@@ -1,49 +1,45 @@
 // @flow
-import React from 'react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
-import { inject, observer } from 'mobx-react'
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
-const enhance = compose(
-  inject('store'),
-  withHandlers({
-    remove: ({ store }) => () => {
-      store.deletionCallback()
-      store.setDeletionCallback(null)
-      store.setDeletionTitle(null)
-      store.setDeletionMessage(null)
-    },
-    close: ({ store }) => () => {
-      store.setDeletionCallback(null)
-      store.setDeletionTitle(null)
-      store.setDeletionMessage(null)
-    }
-  }),
-  observer
-)
+import storeContext from '../storeContext'
 
-const DeletionModal = ({
-  store,
-  remove,
-  close
-}: {
-  store: Object,
-  remove: () => void,
-  close: () => void
-}) => (
-  <Modal isOpen={!!store.deletionMessage} toggle={close}>
-    <ModalHeader toggle={close}>{store.deletionTitle}</ModalHeader>
-    <ModalBody>{store.deletionMessage}</ModalBody>
-    <ModalFooter>
-      <Button color="primary" onClick={remove} outline>
-        ja
-      </Button>
-      <Button color="secondary" onClick={close} outline>
-        nein
-      </Button>
-    </ModalFooter>
-  </Modal>
-)
+const DeletionModal = () => {
+  const store = useContext(storeContext)
+  const {
+    deletionCallback,
+    setDeletionCallback,
+    setDeletionTitle,
+    setDeletionMessage
+  } = store
 
-export default enhance(DeletionModal)
+  const remove = useCallback(() => {
+    deletionCallback()
+    setDeletionCallback(null)
+    setDeletionTitle(null)
+    setDeletionMessage(null)
+  })
+  const close = useCallback(() => {
+    setDeletionCallback(null)
+    setDeletionTitle(null)
+    setDeletionMessage(null)
+  })
+
+  return (
+    <Modal isOpen={!!store.deletionMessage} toggle={close}>
+      <ModalHeader toggle={close}>{store.deletionTitle}</ModalHeader>
+      <ModalBody>{store.deletionMessage}</ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={remove} outline>
+          ja
+        </Button>
+        <Button color="secondary" onClick={close} outline>
+          nein
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
+}
+
+export default observer(DeletionModal)
