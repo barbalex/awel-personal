2190111521788fb079ac8273d4213283c76b418b
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-09 10:32:18 +0100

    use hooks

--- app/components/Navbar/Filter.js
+++ app/components/Navbar/Filter.js
@@ -1,6 +1,6 @@
 // @flow
-import React from 'react'
-import { inject, observer } from 'mobx-react'
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react'
 import styled from 'styled-components'
 import {
   InputGroup,
@@ -9,81 +9,75 @@ import {
   Input,
   UncontrolledTooltip
 } from 'reactstrap'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
+
+import storeContext from '../../storeContext'
 
 const VolltextFilterRemoveAddon = styled(InputGroupText)`
   background-color: white !important;
 `
 
-const enhance = compose(
-  inject('store'),
-  withHandlers({
-    toggleShowFilter: ({ store }) => () => {
-      const { showFilter, setShowFilter } = store
+const Filter = () => {
+  const store = useContext(storeContext)
+  const { showFilter, setShowFilter, filterFulltext, setFilterFulltext } = store
+
+  const toggleShowFilter = useCallback(
+    () => {
       setShowFilter(!showFilter)
     },
-    onChangeFilterFulltext: ({ store }) => e =>
-      store.setFilterFulltext(e.target.value),
-    onEmptyFilterFulltext: ({ store }) => () => store.setFilterFulltext(null)
-  }),
-  observer
-)
+    [showFilter]
+  )
+  const onChangeFilterFulltext = useCallback(e =>
+    setFilterFulltext(e.target.value)
+  )
+  const onEmptyFilterFulltext = useCallback(() => setFilterFulltext(null))
 
-const Filter = ({
-  store,
-  toggleShowFilter,
-  onChangeFilterFulltext,
-  onEmptyFilterFulltext
-}: {
-  store: Object,
-  toggleShowFilter: () => void,
-  onChangeFilterFulltext: () => void,
-  onEmptyFilterFulltext: () => void
-}) => (
-  <div>
-    <InputGroup>
-      <Input
-        placeholder="Volltext filtern"
-        onChange={onChangeFilterFulltext}
-        value={store.filterFulltext || ''}
-      />
-      <InputGroupAddon addonType="append">
-        {store.filterFulltext && (
-          <VolltextFilterRemoveAddon
-            id="volltextFilterRemoveAddon"
-            onClick={onEmptyFilterFulltext}
-          >
-            <i className="fas fa-times" />
-          </VolltextFilterRemoveAddon>
-        )}
-        <InputGroupText id="filterAddon" onClick={toggleShowFilter}>
-          <i className={`fas ${store.showFilter ? 'fa-edit' : 'fa-filter'}`} />
-        </InputGroupText>
-        {store.existsFilter && (
-          <InputGroupText id="emptyFilterAddon" onClick={store.emptyFilter}>
-            <i className="fas fa-times" />
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Volltext filtern"
+          onChange={onChangeFilterFulltext}
+          value={filterFulltext || ''}
+        />
+        <InputGroupAddon addonType="append">
+          {filterFulltext && (
+            <VolltextFilterRemoveAddon
+              id="volltextFilterRemoveAddon"
+              onClick={onEmptyFilterFulltext}
+            >
+              <i className="fas fa-times" />
+            </VolltextFilterRemoveAddon>
+          )}
+          <InputGroupText id="filterAddon" onClick={toggleShowFilter}>
+            <i
+              className={`fas ${store.showFilter ? 'fa-edit' : 'fa-filter'}`}
+            />
           </InputGroupText>
+          {store.existsFilter && (
+            <InputGroupText id="emptyFilterAddon" onClick={store.emptyFilter}>
+              <i className="fas fa-times" />
+            </InputGroupText>
+          )}
+        </InputGroupAddon>
+        {filterFulltext && (
+          <UncontrolledTooltip
+            placement="bottom"
+            target="volltextFilterRemoveAddon"
+          >
+            Volltext-Filter leeren
+          </UncontrolledTooltip>
         )}
-      </InputGroupAddon>
-      {store.filterFulltext && (
-        <UncontrolledTooltip
-          placement="bottom"
-          target="volltextFilterRemoveAddon"
-        >
-          Volltext-Filter leeren
+        <UncontrolledTooltip placement="bottom" target="filterAddon">
+          {store.showFilter ? 'Daten bearbeiten' : 'Nach Felden filtern'}
         </UncontrolledTooltip>
-      )}
-      <UncontrolledTooltip placement="bottom" target="filterAddon">
-        {store.showFilter ? 'Daten bearbeiten' : 'Nach Felden filtern'}
-      </UncontrolledTooltip>
-      {store.existsFilter && (
-        <UncontrolledTooltip placement="bottom" target="emptyFilterAddon">
-          Filter leeren
-        </UncontrolledTooltip>
-      )}
-    </InputGroup>
-  </div>
-)
+        {store.existsFilter && (
+          <UncontrolledTooltip placement="bottom" target="emptyFilterAddon">
+            Filter leeren
+          </UncontrolledTooltip>
+        )}
+      </InputGroup>
+    </div>
+  )
+}
 
-export default enhance(Filter)
+export default observer(Filter)
