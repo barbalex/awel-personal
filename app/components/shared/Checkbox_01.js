969808b88231b528cd81651eb456666252538706
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-09 12:06:47 +0100

    use hooks

--- app/components/shared/Checkbox_01.js
+++ app/components/shared/Checkbox_01.js
@@ -1,9 +1,5 @@
 // @flow
-import React from 'react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
-import withState from 'recompose/withState'
-import withLifecycle from '@hocs/with-lifecycle'
+import React, { useState, useCallback, useEffect } from 'react'
 import { observer } from 'mobx-react'
 import { Col, FormGroup, Label, Input } from 'reactstrap'
 import styled from 'styled-components'
@@ -13,53 +9,49 @@ const StyledInput = styled(Input)`
   top: 10px;
 `
 
-const enhance = compose(
-  withState('stateValue', 'setStateValue', ({ value }) => !!value),
-  withHandlers({
-    onChange: ({ saveToDb, field, stateValue, setStateValue }) => () => {
-      const newValue = !stateValue
-      saveToDb({ value: newValue ? 1 : 0, field })
-      return setStateValue(newValue)
-    }
-  }),
-  withLifecycle({
-    onDidUpdate(prevProps, props) {
-      if (props.value !== prevProps.value) {
-        props.setStateValue(!!props.value)
-      }
-    }
-  }),
-  observer
-)
-
 const SharedCheckbox = ({
   value,
   field,
   label,
-  onChange
+  saveToDb
 }: {
   value: number,
   field: string,
   label: string,
-  onChange: () => void
-}) => (
-  <FormGroup row>
-    <Label for={field} sm={2}>
-      {label}
-    </Label>
-    <Col sm={{ size: 10 }}>
-      <FormGroup check>
-        <Label check>
-          <StyledInput
-            id={field}
-            type="checkbox"
-            checked={value === 1}
-            onChange={onChange}
-          />
-        </Label>
-      </FormGroup>
-    </Col>
-  </FormGroup>
-)
+  saveToDb: () => void
+}) => {
+  const [stateValue, setStateValue] = useState(!!value)
+  const onChange = useCallback(
+    () => {
+      const newValue = !stateValue
+      saveToDb({ value: newValue ? 1 : 0, field })
+      return setStateValue(newValue)
+    },
+    [stateValue, field]
+  )
+  useEffect(() => {
+    setStateValue(!!value)
+  })
 
-export default enhance(SharedCheckbox)
+  return (
+    <FormGroup row>
+      <Label for={field} sm={2}>
+        {label}
+      </Label>
+      <Col sm={{ size: 10 }}>
+        <FormGroup check>
+          <Label check>
+            <StyledInput
+              id={field}
+              type="checkbox"
+              checked={value === 1}
+              onChange={onChange}
+            />
+          </Label>
+        </FormGroup>
+      </Col>
+    </FormGroup>
+  )
+}
+
+export default observer(SharedCheckbox)
