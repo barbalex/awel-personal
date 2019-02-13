877c97d756fae8b8232e5e7d6eebe82a6cd3b40b
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-13 15:44:15 +0100

    sort by keys

--- app/components/Navbar/Export/getDataArrayFromExportObjects.js
+++ app/components/Navbar/Export/getDataArrayFromExportObjects.js
@@ -1,20 +1,22 @@
 export default exportObjects => {
   const dataArray = []
   // first the field names:
-  dataArray.push(Object.keys(exportObjects[0]))
+  dataArray.push(Object.keys(exportObjects[0]).sort())
   // then the field values
   exportObjects.forEach(object =>
     dataArray.push(
-      Object.keys(object).map((key, index) => {
-        /**
-         * exceljs errors out if first member of array is null
-         * see: https://github.com/guyonroche/exceljs/issues/111
-         */
-        if (object[key] === null && index === 0) {
-          return ''
-        }
-        return object[key]
-      }),
+      Object.keys(object)
+        .sort()
+        .map((key, index) => {
+          /**
+           * exceljs errors out if first member of array is null
+           * see: https://github.com/guyonroche/exceljs/issues/111
+           */
+          if (object[key] === null && index === 0) {
+            return ''
+          }
+          return object[key]
+        }),
     ),
   )
   return dataArray
