e80eed97e8ac0f14902e9f677670365177bc5cf9
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-20 15:08:48 +0100

    work on printing pd

--- app/components/Navbar/Berichte.js
+++ app/components/Navbar/Berichte.js
@@ -4,24 +4,37 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Button,
 } from 'reactstrap'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
+import { FaPrint, FaRegFilePdf } from 'react-icons/fa'
 
 import storeContext from '../../storeContext'
+
+const StyledUncontrolledDropdown = styled(UncontrolledDropdown)`
+  display: flex;
+  border: ${props =>
+    props.active ? '1px solid rgb(255, 255, 255, .5)' : 'unset'};
+  border-radius: 0.25rem;
+  margin-right: 5px;
+`
+const StyledButton = styled(Button)`
+  background-color: rgba(0, 0, 0, 0) !important;
+  border: unset !important;
+`
 
 const Berichte = () => {
   const store = useContext(storeContext)
   const { setLocation } = store
   const location = store.location.toJSON()
-  const activeLocation = location[0]
   const showPD = location[0] === 'Personen' && location[1]
   const onClickPD = useCallback(() => {
     setLocation([...location, 'pdf'])
   }, [location])
 
   return (
-    <UncontrolledDropdown nav inNavbar active={activeLocation === 'berichte'}>
+    <StyledUncontrolledDropdown nav inNavbar active={location.includes('pdf')}>
       <DropdownToggle nav caret>
         Berichte
       </DropdownToggle>
@@ -34,7 +47,14 @@ const Berichte = () => {
         )}
         <DropdownItem>mehr?</DropdownItem>
       </DropdownMenu>
-    </UncontrolledDropdown>
+      {location.includes('pdf') && (
+        <>
+          <StyledButton id="newPersonButton">
+            <FaPrint />
+          </StyledButton>
+        </>
+      )}
+    </StyledUncontrolledDropdown>
   )
 }
 
