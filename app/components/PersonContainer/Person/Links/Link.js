1a3ac798f76992e5cc7e1b10b5001502467c1692
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-24 20:22:07 +0100

    style

--- app/components/PersonContainer/Person/Links/Link.js
+++ app/components/PersonContainer/Person/Links/Link.js
@@ -16,9 +16,6 @@ const Field = styled.div`
   padding: 3px;
   align-items: center;
   min-height: 35px;
-  &:first-of-type {
-    border-top: thin solid #cccccc;
-  }
   &:hover {
     background-color: rgba(0, 0, 0, 0.05);
   }
