220bc5221a802552d3dd9b52f11c8a7bcecce50b
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-25 16:30:36 +0100

    add data

--- app/sql/createData.sql
+++ app/sql/createData.sql
@@ -56,20 +56,6 @@ values
   ('extern', 4);
 
 insert into
-  personen(id, vorname, name)
-values
-(1, 'Werner',	'Haas'),
-(2, 'Erich',	'Hess'),
-(3, 'Thomas',	'Hofmann'),
-(4, 'Guido',	'Merletti'),
-(5, 'Peter',	'Wyler'),
-(6, 'Fritz',	'Studer'),
-(7, 'Paul',	'Ruckstuhl'),
-(8, 'Götz',	'Christian'),
-(9, 'Beat',	'Koller'),
-(10, 'Thomas',	'Flüeler');
-
-insert into
   aemter(name, id, kostenstelle)
 values
   ('AWEL', 1, '850000');
@@ -87,29 +73,29 @@ values
   (7, 'Wasserbau', 'wb', 1, '852700');
 
 insert into
-  sektionen(name, kurzzeichen, abteilung, kostenstelle)
+  sektionen(id, name, kurzzeichen, abteilung, kostenstelle)
 values
-  ('Abfallwirtschaft', '', 1, '851210'), -- Abfallwirtschaft und Betriebe
-  ('Altlasten', '', 1, '851220'), -- Abfallwirtschaft und Betriebe
-  ('Betrieblicher Umweltschutz und Störfallvorsorge', '', 1, '851230'), -- Abfallwirtschaft und Betriebe
-  ('Biosicherheit', '', 1, '851240'), -- Abfallwirtschaft und Betriebe
-  ('Energieberatung', '', 3, '853830'), -- Energie
-  ('Energietechnik', '', 3, '853810'), -- Energie
-  ('Energiewirtschaft', '', 3, '853820'), -- Energie
-  ('Oberflächengewässerschutz', '', 4, '852810'), -- Gewässerschutz
-  ('Abwasserreinigungsanlagen', '', 4, '852820'), -- Gewässerschutz
-  ('Grundwasser und Wasserversorgung', '', 4, '852840'), -- Gewässerschutz
-  ('Bevölkerungsschutz', '', 4, '852850'), -- Gewässerschutz
-  ('Siedlungsentwässerung', '', 4, '852830'), -- Gewässerschutz
-  ('Monitoring', '', 5, '851410'), -- Luft
-  ('Emissionskontrolle', '', 5, '851440'), -- Luft
-  ('Klima und Mobilität', '', 5, '851420'), -- Luft
-  ('Strahlung', '', 5, '851430'), -- Luft
-  ('Beratung und Bewilligungen', '', 7, '852770'), -- Wasserbau
-  ('Planung', '', 7, '852730'), -- Wasserbau
-  ('Gewässernutzung', '', 7, '852740'), -- Wasserbau
-  ('Bau', '', 7, '852710'), -- Wasserbau
-  ('Gewässerunterhalt', '', 7, '852760'); -- Wasserbau
+  (1, 'Abfallwirtschaft', '', 1, '851210'), -- Abfallwirtschaft und Betriebe
+  (2, 'Altlasten', '', 1, '851220'), -- Abfallwirtschaft und Betriebe
+  (3, 'Betrieblicher Umweltschutz und Störfallvorsorge', '', 1, '851230'), -- Abfallwirtschaft und Betriebe
+  (4, 'Biosicherheit', '', 1, '851240'), -- Abfallwirtschaft und Betriebe
+  (5, 'Energieberatung', '', 3, '853830'), -- Energie
+  (6, 'Energietechnik', '', 3, '853810'), -- Energie
+  (7, 'Energiewirtschaft', '', 3, '853820'), -- Energie
+  (8, 'Oberflächengewässerschutz', '', 4, '852810'), -- Gewässerschutz
+  (9, 'Abwasserreinigungsanlagen', '', 4, '852820'), -- Gewässerschutz
+  (10, 'Grundwasser und Wasserversorgung', '', 4, '852840'), -- Gewässerschutz
+  (11, 'Bevölkerungsschutz', '', 4, '852850'), -- Gewässerschutz
+  (12, 'Siedlungsentwässerung', '', 4, '852830'), -- Gewässerschutz
+  (13, 'Monitoring', '', 5, '851410'), -- Luft
+  (14, 'Emissionskontrolle', '', 5, '851440'), -- Luft
+  (15, 'Klima und Mobilität', '', 5, '851420'), -- Luft
+  (16, 'Strahlung', '', 5, '851430'), -- Luft
+  (17, 'Beratung und Bewilligungen', '', 7, '852770'), -- Wasserbau
+  (18, 'Planung', '', 7, '852730'), -- Wasserbau
+  (19, 'Gewässernutzung', '', 7, '852740'), -- Wasserbau
+  (20, 'Bau', '', 7, '852710'), -- Wasserbau
+  (21, 'Gewässerunterhalt', '', 7, '852760'); -- Wasserbau
 
   -- ('Tankanlagen und Transportgewerbe', '', 1, '851250'), -- Abfallwirtschaft und Betriebe
   -- ('Finanz und Rechnungswesen', '', 2, ''), -- Dienste
@@ -126,34 +112,18 @@ values
   -- ('Kanzlei AWEL', '', 2, ''), -- Dienste
 
 insert into
-  bereiche(abteilung, sektion, name, standort, leiter)
+  personen(id, vorname, name)
 values
-('Wasserbau',	'Bau',	'Betrieb Sihl',	'Adliswil',	1),
-('Wasserbau',	'Bau',	'Betrieb Reuss / Limmat', 	'Obfelden',	2),
-('Wasserbau',	'Bau',	'Betrieb Töss',	'Hettlingen',	3),
-('Wasserbau',	'Bau',	'Betrieb Thur / Rhein',	'Andelfingen',	4),
-('Wasserbau',	'Bau',	'Betrieb Glatt',	'Glattbrugg',	5),
-('Wasserbau',	'Bau',	'Betrieb Oberland / Seen',	'Hinwil',	6),
-('Gewässerschutz',	'Grundwasser und Wasserversorgung',	'Wasserversorung',	'Stampfenbachstrasse 12 / 14',	7),
-('Gewässerschutz',	'Oberflächengewässerschutz',	'Fachgruppe Analytik',	'Carbahaus',	8),
-('Gewässerschutz',	'Siedlungsentwässerung',	'Liegenschaftenentwässerung',	'Stampfenbachstrasse 12 / 14',	9),
-('Energie',	'',	'Kerntechnik',	'Stampfenbachstrasse 12 / 14',	10);
-
---insert into
---  bereiche(name)
---values
---  ('Kernenergietechnik/Radioaktive Abfälle'),
---  ('Tiefenlager'),
---  ('Bearbeitung von Rechtsfragen'),
---  ('Rekurse und Beschwerden'),
---  ('Juristische Beratung'),
---  ('Rechtliche Vertretung des Amtes nach Aussen'),
---  ('Finanz und Rechnungswesen'),
---  ('Controllerdienst'),
---  ('Qualitäts- und Umweltmanagement'),
---  ('Informatik'),
---  ('Internet'),
---  ('Kanzlei AWEL');
+(1, 'Werner',	'Haas'),
+(2, 'Erich',	'Hess'),
+(3, 'Thomas',	'Hofmann'),
+(4, 'Guido',	'Merletti'),
+(5, 'Peter',	'Wyler'),
+(6, 'Fritz',	'Studer'),
+(7, 'Paul',	'Ruckstuhl'),
+(8, 'Götz',	'Christian'),
+(9, 'Beat',	'Koller'),
+(10, 'Thomas',	'Flüeler');
 
 insert into
   settings(id)
@@ -215,3 +185,33 @@ values
   ('Italien'),
   ('Frankreich'),
   ('Österreich');
+
+insert into
+  bereiche(abteilung, sektion, name, standort, leiter)
+values
+(7,	20,	'Betrieb Sihl',	'Werkhof Adliswil',	1),
+(7,	20,	'Betrieb Reuss / Limmat', 'Werkhof Obfelden',	2),
+(7,	20,	'Betrieb Töss',	'Werkhof Hettlingen',	3),
+(7,	20,	'Betrieb Thur / Rhein',	'Werkhof Andelfingen',	4),
+(7,	20,	'Betrieb Glatt',	'Werkhof Glattbrugg',	5),
+(7,	20,	'Betrieb Oberland / Seen',	'Werkhof Hinwil',	6),
+(4,	10,	'Wasserversorung',	'Stampfenbachstrasse 12/14',	7),
+(4,	8,	'Fachgruppe Analytik',	'Carbahaus',	8),
+(4,	12,	'Liegenschaftenentwässerung',	'Stampfenbachstrasse 12/14',	9),
+(3,	null,	'Kerntechnik',	'Stampfenbachstrasse 12/14',	10);
+
+--insert into
+--  bereiche(name)
+--values
+--  ('Kernenergietechnik/Radioaktive Abfälle'),
+--  ('Tiefenlager'),
+--  ('Bearbeitung von Rechtsfragen'),
+--  ('Rekurse und Beschwerden'),
+--  ('Juristische Beratung'),
+--  ('Rechtliche Vertretung des Amtes nach Aussen'),
+--  ('Finanz und Rechnungswesen'),
+--  ('Controllerdienst'),
+--  ('Qualitäts- und Umweltmanagement'),
+--  ('Informatik'),
+--  ('Internet'),
+--  ('Kanzlei AWEL');
