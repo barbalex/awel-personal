b29f085f6d2b22175ed4802096b66a4c0242a16e
Alexander Gabriel <alex.barbalex@gmail.com>
2019-02-25 21:48:38 +0100

    add new version

--- app/components/Navbar/More.js
+++ app/components/Navbar/More.js
@@ -113,7 +113,7 @@ const More = () => {
           Fehler und Wünsche melden
         </DropdownItem>
         <DropdownItem divider />
-        <Version>Version: 0.20.0 vom 12.02.2019</Version>
+        <Version>Version: 0.21.0 vom 25.02.2019</Version>
       </DropdownMenu>
     </MoreMenu>
   )
