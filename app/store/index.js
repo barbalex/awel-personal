209c05b160de00521d0546dd2af61a79a3c6799d
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-22 18:24:51 +0100

    sort by mutation nötig

--- app/store/index.js
+++ app/store/index.js
@@ -435,6 +435,21 @@ export default db =>
             )
           })
           .sort((a, b) => {
+            if (self.showMutationNoetig) {
+              if (a.mutationFrist && b.mutationFrist) {
+                const aDate = new Date(a.mutationFrist)
+                const bDate = new Date(b.mutationFrist)
+                return aDate - bDate
+              } else if (a.mutationFrist) {
+                return -1
+              } else if (b.mutationFrist) {
+                return 1
+              } else if (a.mutationNoetig && !b.mutationNoetig) {
+                return -1
+              } else if (!a.mutationNoetig && b.mutationNoetig) {
+                return 1
+              }
+            }
             if (!a.name && !a.vorname) return -1
             if (a.name && b.name && a.name.toLowerCase() < b.name.toLowerCase())
               return -1
