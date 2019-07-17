23a17355f77c0f284779a83d71c470b3b7db1064
Alexander Gabriel <alex.barbalex@gmail.com>
2019-07-17 16:15:23 +0200

    get headings right

--- app/src/idWithCapitalMapFromPers.js
+++ app/src/idWithCapitalMapFromPers.js
@@ -40,12 +40,15 @@ export default ({ personen, field = 'name' }) => {
       ? sortBy(personen, p => p.kurzzeichen.toLowerCase())
       : personen
   personenToUse.forEach(p => {
-    const firstChar = p[field].charAt(0).toUpperCase()
-    while (firstChar >= alphabet[0]) {
+    const firstChar = (p[field] || '').charAt(0).toLowerCase()
+    while (
+      !!alphabet.length &&
+      alphabet[0].toLowerCase().localeCompare(firstChar, 'de-Ch') <= 0
+    ) {
       res.push(alphabet.shift())
     }
     res.push(p.id)
   })
   // add unused from alphabet
-  return [...res, ...alphabet].filter(r => r !== undefined)
+  return [...res, ...alphabet]
 }
