cf515edbb363ea2956442af6438959430a524819
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-23 15:08:58 +0200

    update

--- src/store/personenSorted.js
+++ src/store/personenSorted.js
@@ -1,6 +1,8 @@
-export default personen =>
+const personenSorted = (personen) =>
   personen.slice().sort((a, b) => {
     const nameCompared = (a.name || '').localeCompare(b.name || '', 'de-Ch')
     if (nameCompared !== 0) return nameCompared
     return (a.vorname || '').localeCompare(b.vorname || '', 'de-Ch')
   })
+
+export default personenSorted
