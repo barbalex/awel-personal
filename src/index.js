5ac46b25bd885d3a2d038431f8caf049dba0bd2d
Alexander Gabriel <alex.barbalex@gmail.com>
2021-08-27 10:23:56 +0200

    load on start

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ import getDb from './src/getDb'
 import createStore from './store'
 import watchMutations from './src/watchMutations'
 
+import fetchPersonen from './src/fetchPersonen'
+import fetchAemter from './src/fetchAemter'
+import fetchAbteilungen from './src/fetchAbteilungen'
+import fetchBereiche from './src/fetchBereiche'
+import fetchSektionen from './src/fetchSektionen'
+import fetchEtiketten from './src/fetchEtiketten'
+import fetchAnwesenheitstage from './src/fetchAnwesenheitstage'
+import fetchLinks from './src/fetchLinks'
+import fetchSchluessel from './src/fetchSchluessel'
+import fetchMobileAbos from './src/fetchMobileAbos'
+import fetchTelefones from './src/fetchTelefones'
+import fetchFunktionen from './src/fetchFunktionen'
+import fetchKaderFunktionen from './src/fetchKaderFunktionen'
+import fetchWerte from './src/fetchWerte'
+import fetchSettings from './src/fetchSettings'
+
 import { StoreContextProvider } from './storeContext'
 
 const run = async () => {
@@ -27,6 +43,35 @@ const run = async () => {
   if (user) setUsername(user)
 
   window.store = store
+
+  fetchPersonen({ store })
+  fetchAemter({ store })
+  fetchAbteilungen({ store })
+  fetchBereiche({ store })
+  fetchSektionen({ store })
+  fetchWerte({ store, table: 'statusWerte' })
+  fetchWerte({ store, table: 'anredeWerte' })
+  fetchWerte({ store, table: 'funktionWerte' })
+  fetchWerte({ store, table: 'kaderFunktionWerte' })
+  fetchEtiketten({ store })
+  fetchAnwesenheitstage({ store })
+  fetchWerte({ store, table: 'etikettWerte' })
+  fetchWerte({ store, table: 'anwesenheitstagWerte' })
+  fetchWerte({ store, table: 'landWerte' })
+  fetchWerte({ store, table: 'mutationArtWerte' })
+  fetchWerte({ store, table: 'standortWerte' })
+  fetchLinks({ store })
+  fetchSchluessel({ store })
+  fetchMobileAbos({ store })
+  fetchTelefones({ store })
+  fetchWerte({ store, table: 'mobileAboKostenstelleWerte' })
+  fetchWerte({ store, table: 'mobileAboTypWerte' })
+  fetchWerte({ store, table: 'telefonTypWerte' })
+  fetchWerte({ store, table: 'schluesselTypWerte' })
+  fetchWerte({ store, table: 'schluesselAnlageWerte' })
+  fetchFunktionen({ store })
+  fetchKaderFunktionen({ store })
+  fetchSettings({ store })
 
   render(
     <StoreContextProvider value={store}>
