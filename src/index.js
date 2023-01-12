d256ed78903db998602f00ae960ad2cd01131ebc
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-12 22:33:27 +0100

    clean

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { render } from 'react-dom'
 import { ipcRenderer } from 'electron'
 
 import App from './components/App'
